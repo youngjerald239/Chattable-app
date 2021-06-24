ed3528947261b87b5b94269eecff8247f0c0e981
youngjerald239 <youngjerald239@gmail.com>
2021-06-24 18:39:00 -0500

    completed widgets section

--- src/Widgets.js
+++ src/Widgets.js
@@ -11,7 +11,7 @@ function Widgets() {
             <input placeholder="Search Chattable" type="text"/>
            </div>
            <div className="widgets__widgetContainer">
-               <h2>What's on your mind?</h2>
+               <h2>What's happening?</h2>
                <TwitterTweetEmbed tweetId={"1407409199378751491"}/>
                <TwitterTimelineEmbed sourceType="profile" screenName="Toritoomuch" options={{height:600}}/>
                <TwitterShareButton url={"https://twitter.com/ToriTooMuch?ref_src=twsrc%5Etfw%7Ctwcamp%5Etweetembed%7Ctwterm%5E1407409199378751491%7Ctwgr%5E%7Ctwcon%5Es1_&ref_url=https%3A%2F%2Fpublish.twitter.com%2F%3Fquery%3Dhttps3A2F2Ftwitter.com2FToriTooMuch2Fstatus2F1407409199378751491widget%3DTweet"} options={{text:"#reactjs is awesome", via:"Jerald Young"}}/>
