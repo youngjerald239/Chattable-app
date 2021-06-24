35bcf250bc8b486b8238213c49990104d37e892c
youngjerald239 <youngjerald239@gmail.com>
2021-06-23 19:08:25 -0500

    added sidebar icons and text

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -2,6 +2,14 @@ import React from 'react'
 import "./Sidebar.css"
 import QuestionAnswerTwoToneIcon from '@material-ui/icons/QuestionAnswerTwoTone';
 import SidebarOption from './SidebarOption';
+import HomeWorkTwoToneIcon from '@material-ui/icons/HomeWorkTwoTone';
+import SearchTwoToneIcon from '@material-ui/icons/SearchTwoTone';
+import NotificationsNoneTwoToneIcon from '@material-ui/icons/NotificationsNoneTwoTone';
+import MailTwoToneIcon from '@material-ui/icons/MailTwoTone';
+import BookmarkBorderTwoToneIcon from '@material-ui/icons/BookmarkBorderTwoTone';
+import ListAltTwoToneIcon from '@material-ui/icons/ListAltTwoTone';
+import MoreHorizTwoToneIcon from '@material-ui/icons/MoreHorizTwoTone';
+import PermIdentityTwoToneIcon from '@material-ui/icons/PermIdentityTwoTone';
 
 function Sidebar() {
     return (
@@ -10,14 +18,14 @@ function Sidebar() {
                {/* Chattable icon */}
             <QuestionAnswerTwoToneIcon/>
                
-            <SidebarOption/>
-            <SidebarOption/>
-            <SidebarOption/>
-            <SidebarOption/>
-               {/* SidebarOption */}
-               {/* SidebarOption */}
-               {/* SidebarOption */}
-               {/* SidebarOption */}
+            <SidebarOption Icon={HomeWorkTwoToneIcon} text="Home"/>
+            <SidebarOption Icon={SearchTwoToneIcon} text="Explore"/>
+            <SidebarOption Icon={NotificationsNoneTwoToneIcon} text="Notifications"/>
+            <SidebarOption Icon={MailTwoToneIcon} text="Email"/>
+            <SidebarOption Icon={BookmarkBorderTwoToneIcon} text="Bookmarks"/>
+            <SidebarOption Icon={ListAltTwoToneIcon} text="Lists"/>
+            <SidebarOption Icon={PermIdentityTwoToneIcon} text="User"/>
+            <SidebarOption Icon={MoreHorizTwoToneIcon} text="More"/>
                {/* SidebarOption */}
                {/* SidebarOption */}
 
